b9fa65855ec97e2940f15b25235b164e03027d77
yassinebha <yanamarji@gmail.com>
2016-07-15 17:33:21 -0400

    up

--- generate_images.m
+++ generate_images.m
@@ -1,7 +1,7 @@
 % generate images for zooniverse platform
-path_preproc = '/media/yassinebha/database24/MAVEN_06_2016/fmri_preprocess_INSCAPE_REST/';
+path_preproc = '/media/yassinebha/database24/adhd200/';
 files_in = niak_grab_qc_fmri_preprocess(path_preproc);
-folder_out = '/media/yassinebha/database24/MAVEN_06_2016/qc_report2/';
+folder_out = [path_preproc '/qc_report2/'];
 folder_out_zooniv = [folder_out 'QC_zooniverse/'];
 niak_mkdir(folder_out_zooniv);
 coord_select = [-30 , -65 , -15 ; 
@@ -23,41 +23,41 @@ n_shift = 0;
 for ss = 1:length(fieldnames(files_in.func))
     subj_id = fieldnames(files_in.func){ss};
     
-%    %% Generate the image for the structural scan
-%    fprintf('%s : Generating slices of the anatomical scan...\n',subj_id);
-%    in_a.source = files_in.anat.(subj_id);
-%    in_a.target = files_in.template;
-%    out_a = [folder_out 'summary_' subj_id '_anat.jpg'];
-%    opt_a.coord = coord_select;
-%    opt_a.colorbar = false;
-%    opt_a.colormap = 'gray';
-%    opt_a.limits = 'adaptative';
-%    opt_a.title = '';
-%    opt_a.flag_decoration = false;
-%    niak_brick_vol2img(in_a,out_a,opt_a);
-%    
-%    %% Generate the image for the functional scan
-%   
-%    fprintf('%s : Generating slices of the functional scan...\n',subj_id);
-%    in_f.source = files_in.func.(subj_id);
-%    in_f.target = files_in.template;
-%    out_f = [folder_out 'summary_' subj_id '_func.jpg'];
-%    opt_f.coord = coord_select;
-%    opt_f.colorbar = false;
-%    opt_f.colormap = 'jet';
-%    opt_f.limits = 'adaptative';
-%    opt_f.title = '';
-%    opt_f.flag_decoration = false;
-%    niak_brick_vol2img(in_f,out_f,opt_f);
+    %% Generate the image for the structural scan
+    fprintf('%s : Generating slices of the anatomical scan...\n',subj_id);
+    in_a.source = files_in.anat.(subj_id);
+    in_a.target = files_in.template;
+    out_a = [folder_out 'summary_' subj_id '_anat.jpg'];
+    opt_a.coord = coord_select;
+    opt_a.colorbar = false;
+    opt_a.colormap = 'gray';
+    opt_a.limits = 'adaptative';
+    opt_a.title = '';
+    opt_a.flag_decoration = false;
+    niak_brick_vol2img(in_a,out_a,opt_a);
+    
+    %% Generate the image for the functional scan
+   
+    fprintf('%s : Generating slices of the functional scan...\n',subj_id);
+    in_f.source = files_in.func.(subj_id);
+    in_f.target = files_in.template;
+    out_f = [folder_out 'summary_' subj_id '_func.jpg'];
+    opt_f.coord = coord_select;
+    opt_f.colorbar = false;
+    opt_f.colormap = 'jet';
+    opt_f.limits = 'adaptative';
+    opt_f.title = '';
+    opt_f.flag_decoration = false;
+    niak_brick_vol2img(in_f,out_f,opt_f);
 
     ## Morph anat and template
-%    system(['convert ' folder_out 'summary_template.jpg  ' folder_out 'summary_' subj_id '_anat.jpg ' folder_out 'summary_template.jpg -delay 1 -morph 3 ' folder_out_zooniv 'morph_' subj_id '_anat_template.gif']);
-%    ## Resize image
-%    system(['mogrify -resize 315x353 ' folder_out_zooniv 'morph_' subj_id '_anat_template.gif']);
-%    ## Morph anat and func
-%    system(['convert  ' folder_out 'summary_' subj_id '_anat.jpg  ' folder_out 'summary_' subj_id '_func.jpg ' folder_out 'summary_' subj_id '_anat.jpg -delay 1 -morph 3 ' folder_out_zooniv 'morph_' subj_id '_anat_func.gif']);
-%    ## Resize image
-%    system(['mogrify -resize 310x348 ' folder_out_zooniv 'morph_' subj_id '_anat_func.gif']);
+    system(['convert ' folder_out 'summary_template.jpg  ' folder_out 'summary_' subj_id '_anat.jpg ' folder_out 'summary_template.jpg -delay 1 -morph 3 ' folder_out_zooniv 'morph_' subj_id '_anat_template.gif']);
+    ## Resize image
+    system(['mogrify -resize 315x353 ' folder_out_zooniv 'morph_' subj_id '_anat_template.gif']);
+    ## Morph anat and func
+    system(['convert  ' folder_out 'summary_' subj_id '_anat.jpg  ' folder_out 'summary_' subj_id '_func.jpg ' folder_out 'summary_' subj_id '_anat.jpg -delay 1 -morph 3 ' folder_out_zooniv 'morph_' subj_id '_anat_func.gif']);
+    ## Resize image
+    system(['mogrify -resize 310x348 ' folder_out_zooniv 'morph_' subj_id '_anat_func.gif']);
     
     ## Create manifest csv file
     
